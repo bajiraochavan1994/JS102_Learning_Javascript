d69a6e6be9bb2a21bed72468c9a4573ba3b001ba
bajirao chavan <bajiraochavan1994@gmail.com>
2024-02-02 19:12:37 +0530

    add while loop file

---
+++ problem4.js
@@ -0,0 +1,9 @@
+var n = 50;
+
+var i =1;
+while(i<=n){
+  if(i%2!==0){
+    console.log(i)
+  }
+  i++
+}
---
+++ problem5.js
@@ -0,0 +1,8 @@
+var n = 50;
+// use while loop
+
+var i = 1;     
+while(i<=n){
+  console.log(i)
+  i++
+}
